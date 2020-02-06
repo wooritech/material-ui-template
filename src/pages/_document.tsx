1b6bbb7baff2dea14d13a0769ce73747342f5aaa
onlydel <onlydel@realgrid.com>
2020-02-06 16:41:19 +0900

    install  package: material-table

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,8 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          {/* material-table */}
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         </Head>
         <body>
           <Main />
